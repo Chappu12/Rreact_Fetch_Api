cbdac944087a40b782c6615d968e8a07cc22a349
prathamesh <prathamesh69bhavsar@gmail.com>
2025-01-03 12:34:34 +0530

    something is happning

--- src/Components/Nasa-API/Nasa.js
+++ src/Components/Nasa-API/Nasa.js
@@ -5,18 +5,20 @@ const Nasa = () => {
 
     useEffect(() => {
         fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY")
-            .then((response) => response.json()) // Corrected
-            .then((data) => setMars(data)); // Removed unnecessary braces
+            .then((response) => response.json()) 
+            .then((data) => setMars(data)); 
     }, []);
 
     return (
-        <div className='flex w-screen bg-gray-600 overflow-x-hidden'>
-            {mars.photos.map((item) => (
-                <div className='m-2 p-4 flex bg-black w-20 text-sky-400'>
-                    <p key={item.id}>{item.id}</p> 
-                </div>
-            ))}
-        </div>
+        <>
+            <div className='flex w-screen bg-gray-600 overflow-x-hidden'>
+                {mars.photos.map((item) => (
+                    <div className='m-2 p-4 flex bg-black w-20 text-sky-400'>
+                        <p key={item.id}>{item.id}</p>
+                    </div>
+                ))}
+            </div>
+        </>
     );
 };
 

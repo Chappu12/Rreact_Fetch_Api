c03a06c806de745b9f518ac40b2b75b65c588208
prathamesh <prathamesh69bhavsar@gmail.com>
2025-01-03 12:18:46 +0530

    tailwind added

---
+++ src/Components/Nasa-API/Nasa.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+
+const Nasa = () => {
+    const [mars, setMars] = useState({ photos: [] });
+
+    useEffect(() => {
+        fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY")
+            .then((response) => response.json()) // Corrected
+            .then((data) => setMars(data)); // Removed unnecessary braces
+    }, []);
+
+    return (
+        <div className='flex '>
+            {mars.photos.map((item) => (
+                <div className='m-2 p-4 flex bg-black w-20 '>
+                    <p key={item.id} className="text-sky-400">{item.id}</p> 
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Nasa;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Nasa from './Components/Nasa-API/Nasa';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Nasa />
   </React.StrictMode>
 );
 
